47ba7c248f4ac3029b7ce98c8a7da7ebcff07eea
Josip <ujdur.jo@gmail.com>
2026-02-04 21:37:22 +0100

    Zadatak

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,140 @@
+/*
+Kreirati novi Java projekt koji će sadržavati perzistentnu klasu „User” s varijablama „id” (Long), „username” (String) i „email” (String) te
+odgovarajuću konfiguraciju u datoteci „persistence.xml”
+Kreirati novu klasu s „main” metodom koja će sadržavati metode „persistUser”, „detachUser”, „reattachUser” i „deleteUser”
+Metoda „persistUser” mora korištenjem EntityManager i EntityTransaction klasa pozivati metodu „persist” i perzistirati jedan primjer objekta klase „User”
+Metoda „detachUser” mora na sličan način pozvati metodu „detach”, a metoda „reattach” mora pozivom metode „merge” ponovno povezati odvojeni entitet
+Metodom „remove” na sličan način i obrisati objekt klase „User”
+
+
+
+*/
+
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.example.model.User;
+
+public class Main {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("data");
+
+    public static void main(String[] args) {
+
+
+        User user1 = new User("Josip Josipović", "jo@mail.com");
+        persistUser(user1);
+        System.out.println("User persuisted: " + user1);
+
+        detachUser(user1);
+        System.out.println("Detached user: " + user1.getId());
+
+        User reattachedUser = reattachUser(user1);
+        System.out.println("Reattached user: " + reattachedUser);
+
+        deleteUser(reattachedUser);
+        System.out.println("Deleted user: " + user1);
+
+
+    }
+
+    private static void persistUser(User user) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+// Spremanje entiteta u bazu
+            em.persist(user);
+            transaction.commit();
+            System.out.println("User persisted successfully!");
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    private static void detachUser(User user) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+
+            User managedUser = em.find(User.class, user.getId());
+            if (managedUser != null) {
+                em.detach(managedUser);
+                System.out.println("User detached successfully!");
+
+
+                System.out.println("User persisted successfully!");
+
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    private static User reattachUser(User detachedUser) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        User mergedUser = null;
+        try {
+            transaction.begin();
+
+            mergedUser = em.merge(detachedUser);
+            System.out.println("User reattached (merged) successfully!");
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        return mergedUser;
+    }
+
+    private static void deleteUser(User user) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            User delUser = em.find(User.class, user.getId());
+            if (delUser != null) {
+
+                em.remove(delUser);
+                System.out.println("User deleted successfully!");
+            }
+
+            transaction.commit();
+            System.out.println("User persisted successfully!");
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
         }
     }
 }
--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,4 +1,57 @@
-package org.example;
+//Kreirati novi Java projekt koji će sadržavati perzistentnu klasu „User” s varijablama „id” (Long), „username” (String) i „email” (String) te
+//odgovarajuću konfiguraciju u datoteci „persistence.xml”
 
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", username='" + username + "', email='" + email + "'}";
+    }
 }
